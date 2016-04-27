5f8781ea0616f979ade94706800edb4259b816c5
Andrew Perry <ajperry@pansapiens.com>
2016-04-27 11:30:23 +1000

    Make install script play nicely with a pre-baked bio-ansible image, and retry once on failure.

--- BioAnsible/package/Resources/scripts/runAnsible.sh
+++ BioAnsible/package/Resources/scripts/runAnsible.sh
@@ -11,17 +11,57 @@
 #  License for the specific language governing permissions and limitations
 #  under the License.
 
+# BIOANSIBLE_REPO=https://github.com/MonashBioinformaticsPlatform/bio-ansible.git
+BIOANSIBLE_REPO=https://github.com/pansapiens/bio-ansible.git
+
 sudo apt-get update
 sudo iptables -I INPUT 1 -p tcp -m tcp --dport 22 -j ACCEPT -m comment --comment "by murano, BioAnsible"
 
-sudo apt-get -y -q install python-dev python-pip
+sudo apt-get -y -q install python-dev python-pip git build-essential
 sudo pip install ansible markupsafe
+
+# /software and /references live on an external volume (they are symlinks to /mnt/).
+# /software/source lives on the ephemeral volume.
+# The contents of /software/source may be pre-populated before Ansible runs by
+# moving a special directory of packages baked into the image (below)
+sudo mkdir -p /mnt/software
+sudo ln -s /mnt/software /software
+sudo mkdir -p /mnt/references
+sudo ln -s /mnt/references /references
+# if a special directory containing source packages existing in the image, we
+# move it to where bio-ansible will expect it
+if [-d "/bioansible_source_packages" ]; then
+  sudo mv -f /bioansible_source_packages /software/source
+fi
+
 cd /home/ubuntu
-# sudo git clone https://github.com/MonashBioinformaticsPlatform/bio-ansible.git
-sudo git clone https://github.com/pansapiens/bio-ansible.git
-cd bio-ansible
-# sudo ansible-galaxy install -r requirements.yml -p roles/
-sudo ansible-playbook --become-user=root --become -u ubuntu -i "localhost," --connection=local main.yml
+# clone or pull the latest version of bioansible
+if [ ! -d "/home/ubuntu/bio-ansible" ]; then
+  git clone ${BIOANSIBLE_REPO}
+  cd bio-ansible
+else
+  cd bio-ansible
+  git fetch --all
+  git reset --hard origin/master
+  git pull origin master
+fi
+
+cat << EOF >ansible.cfg
+[defaults]
+roles_path = roles/
+log_path=/home/ubuntu/bio-ansible/ansible.log
+EOF
+
+mkdir -p roles
+ansible-galaxy install -r requirements.yml -p roles/
+
+sudo ansible-playbook --become-user=root --become -u ubuntu -i "localhost," --connection=local main.yml 2>&1 | tee ansible.out
+# just retry once if exit code is non-zero
+if [ $? -ne 0 ]; then
+  sudo ansible-playbook --become-user=root --become -u ubuntu -i "localhost," --connection=local main.yml 2>&1 | tee ansible.out
+fi
+
+
 # TODO: pull references and BLAST databases (existing scripts or somewhere else ?)
 # TODO: deal with GATK download - this probably needs to be a post install message in the terminal upon
 #       first login (eg, "please go here, download GATK to tarballs/ then run XXX")
