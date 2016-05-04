30d58d6124fcf9ec8b65cd8569c5aec9de1755ea
Andrew Perry <ajperry@pansapiens.com>
2016-05-04 11:39:08 +1000

    Fix creation of nested duplicate symlinks, add WIP automatic download of reference genomes.

--- BioAnsible/package/Resources/scripts/runAnsible.sh
+++ BioAnsible/package/Resources/scripts/runAnsible.sh
@@ -13,6 +13,10 @@
 
 # BIOANSIBLE_REPO=https://github.com/MonashBioinformaticsPlatform/bio-ansible.git
 BIOANSIBLE_REPO=https://github.com/pansapiens/bio-ansible.git
+# this should match the sw-installer user used in the Ansible playbook
+SW_INSTALLER_USER=sw-installer
+# zero = don't automatically download reference genomes and databases
+DOWNLOAD_REFERENCES=1
 
 sudo apt-get update
 sudo iptables -I INPUT 1 -p tcp -m tcp --dport 22 -j ACCEPT -m comment --comment "by murano, BioAnsible"
@@ -22,17 +26,20 @@ sudo pip install ansible markupsafe
 
 # /software and /references live on an external volume (they are symlinks to /mnt/).
 # /software/source lives on the ephemeral volume.
-# The contents of /software/source may be pre-populated before Ansible runs by
-# moving a special directory of packages baked into the image (below)
 sudo mkdir -p /mnt/software
-sudo ln -s /mnt/software /software
+chown -R ${SW_INSTALLER_USER}.adm /software
+sudo ln -shf /mnt/software /software
 sudo mkdir -p /mnt/references
-sudo ln -s /mnt/references /references
-# if a special directory containing source packages existing in the image, we
-# move it to where bio-ansible will expect it
-if [-d "/bioansible_source_packages" ]; then
-  sudo mv -f /bioansible_source_packages /software/source
-fi
+chown -R ${SW_INSTALLER_USER}.adm /references
+sudo ln -shf /mnt/references /references
+
+# The contents of /software/source may be pre-populated before Ansible runs by
+# moving a special directory of packages baked into the image. If the special
+# directory containing source packages existing in the image, we move it to
+# where bio-ansible will expect it.
+#if [-d "/bioansible_source_packages" ]; then
+#  sudo mv -f /bioansible_source_packages /software/source
+#fi
 
 cd /home/ubuntu
 # clone or pull the latest version of bioansible
@@ -55,14 +62,30 @@ EOF
 mkdir -p galaxy-roles
 ansible-galaxy install -r requirements.yml -p galaxy-roles/
 
-sudo ansible-playbook --become-user=root --become -u ubuntu -i "localhost," --connection=local main.yml 2>&1 | tee ansible.out
+sudo nohup ansible-playbook --become-user=root --become -u ubuntu -i "localhost," --connection=local main.yml 2>&1 | tee ansible.out &
+#sudo ansible-playbook --become-user=root --become -u ubuntu -i "localhost," --connection=local main.yml 2>&1 | tee ansible.out
 # just retry once if exit code is non-zero
-if [ $? -ne 0 ]; then
-  sudo ansible-playbook --become-user=root --become -u ubuntu -i "localhost," --connection=local main.yml 2>&1 | tee ansible.out
-fi
+#if [ $? -ne 0 ]; then
+#  sudo ansible-playbook --become-user=root --become -u ubuntu -i "localhost," --connection=local main.yml 2>&1 | tee ansible.out
+#fi
 
-
-# TODO: pull references and BLAST databases (existing scripts or somewhere else ?)
 # TODO: deal with GATK download - this probably needs to be a post install message in the terminal upon
 #       first login (eg, "please go here, download GATK to tarballs/ then run XXX")
+
+if [ ${DOWNLOAD_REFERENCES} -eq 0 ]; then
+  # Download databases and reference genomes
+  cd scripts
+  sudo -u ${SW_INSTALLER_USER} download-gencode.sh
+  sudo -u ${SW_INSTALLER_USER} download-igenomes.sh
+  sudo -u ${SW_INSTALLER_USER} download-uniprot.sh
+  sudo -u ${SW_INSTALLER_USER} download-rseqc-ref.sh /references/RSeQC
+
+  # Download NCBI-BLAST databases
+  source /software/apps/lmod/lmod/init/bash
+  mkdir /references/blast
+  chown sw-installer.adm /references/*
+  cd /references/blast
+  sudo -u ${SW_INSTALLER_USER} $(which update_blastdb.pl) --passive --verbose '.*'
+fi
+
 # sudo reboot
